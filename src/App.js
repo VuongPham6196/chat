d258c0f24bb326410d70485f17565aaea7ad3e48
vuongpham <phamnhuvuong96@gmail.com>
2022-11-19 21:17:51 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import Home from './pages/Home';
 import './style.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from './context/auth-context';
+
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProdtectRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/">
+            <Route
+              index
+              element={
+                <ProdtectRoute>
+                  <Home />
+                </ProdtectRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
